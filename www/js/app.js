a2484ffde1208961fbff7e9f31597cdc370c9f1d
Brian Noah <brian.noah@jhilburn.com>
2013-10-28 09:09:36 -0700

    Event admin

--- www/js/app.js
+++ www/js/app.js
@@ -70,7 +70,7 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 app.controller("EventAdminController", ['$scope', '$location', '$http', 'apiCall', function($scope, $location, $http, apiCall) {
 	var event_name = $location.path().split('/')[1];
 	scope = $scope;
-
+	$scope.available_graphs = ['Pie', 'Doughnut'];
 	console.log(event_name);
 	apiCall.getEvent(event_name).then(function(result) {
   		angular.extend($scope, result.data[0]);
